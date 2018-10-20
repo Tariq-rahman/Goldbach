a0cc37110f36fb26f709132131713a655f778787
Unknown <Tariqurahman@gmail.com>
2018-10-20 19:16:20 +0100

    Almost working

--- src/Golbach.java
+++ src/Golbach.java
@@ -7,8 +7,6 @@ public class Golbach {
 	public static void main(String[] args) {
 	
 		Golbach golbach  = new Golbach();
-		
-		golbach.populatePrimes();
 
 	}
 	
@@ -16,8 +14,15 @@ public class Golbach {
 		primeNumbers = new ArrayList<>();
 		primeNumbers.add(2);
 		primeNumbers.add(3);
+		primeNumbers.add(5);
 	}
 	
+	/**
+	 * Checks if is odd.
+	 *
+	 * @param number the int
+	 * @return true, if is odd
+	 */
 	public boolean isOdd(int number){
 		
 		if (number % 2 == 0){
@@ -28,28 +33,70 @@ public class Golbach {
 		}
 	}
 	
+	/**
+	 * Checks if is decimal.
+	 *
+	 * @param number the int
+	 * @return true, if is decimal
+	 */
+	public boolean isDecimal(int number){
+		if (number % 1 == 0){
+			return false;	
+	}
+		else {
+			return true;
+		}
+	}
+	
+	
 	public boolean isPrime(int number){
 		
-		if(number % 1 == 0){
+		if(primeNumbers.contains(number)){
+			return true;
+		}
+		else if(!isOdd(number)){
+			//no even number can be prime bar 2
 			return false;
 		}
 		else{
-			return true;
+			for(int i = 3; i < number/2; i++){
+				if (!isDecimal(number/i)){
+					//if division returns Integer then it is not prime
+					return  false;
+				}
+				i++;
+				//incrementing by 2 each loop to only divide by odd numbers
+			}
 		}
+		
+		return true;
 		
 	}
 	
 	public void process(int oddNumber){
 		
 		boolean processing = true;
+		int indexZ = 0;
+		int indexY = 0;
+		Boolean varLastIncremented = true;
 		while(processing){
 			
-			int n = oddNumber - ()
+			int n = oddNumber - (primeNumbers.get(indexY) + (primeNumbers.get(indexZ)));
+			
+			if(isPrime(n)){
+				processing = false;
+				outputResults(n, primeNumbers.get(indexY), primeNumbers.get(indexZ));
+			}
+			else if(varLastIncremented){
+				indexY++;
+			}
+			else {
+				indexZ++;
+			}
 		}
 	}
-	
-	public void populatePrimes(){
-		
-	}
 
+	public String outputResults(int prime1, int prime2, int prime3){
+		return "Here are the numbers " + prime1 + " " + prime2 + " " + prime3;
+	}
 }
