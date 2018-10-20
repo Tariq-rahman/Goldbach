2d516c984a87a5bcc63a321041950f5a6ff4a015
Unknown <Tariqurahman@gmail.com>
2018-10-20 15:48:03 +0100

    Created Methods
    + made isPrime
    + made isOdd
    + made constructor
    + started on process()
    + created primeNumbers arraylist

--- src/Golbach.java
+++ src/Golbach.java
@@ -1,20 +1,54 @@
+import java.util.ArrayList;
 
 public class Golbach {
+	
+	private ArrayList<Integer> primeNumbers;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Golbach golbach  = new Golbach();
+		
+		golbach.populatePrimes();
 
 	}
 	
-	public boolean isOdd(){
+	public Golbach(){
+		primeNumbers = new ArrayList<>();
+		primeNumbers.add(2);
+		primeNumbers.add(3);
+	}
+	
+	public boolean isOdd(int number){
+		
+		if (number % 2 == 0){
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
+	public boolean isPrime(int number){
+		
+		if(number % 1 == 0){
+			return false;
+		}
+		else{
+			return true;
+		}
 		
 	}
 	
-	public boolean isPrime(){
+	public void process(int oddNumber){
 		
+		boolean processing = true;
+		while(processing){
+			
+			int n = oddNumber - ()
+		}
 	}
 	
-	public void process(){
+	public void populatePrimes(){
 		
 	}
 
